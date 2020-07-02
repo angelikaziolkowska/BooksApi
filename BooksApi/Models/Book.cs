f7beac83b6a14be302062a087e91924476d2ab2e
Angelika Ziolkowska <angelikaenquiries@gmail.com>
2020-07-02 04:15:51 +0100

    Add files via upload

---
+++ BooksApi/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+
+namespace BooksApi.Models
+{
+    public class Book
+    {
+        [BsonId] //primary key in MongoDB
+        [BsonRepresentation(BsonType.ObjectId)] //allows passing as string instead of an ObjectId structure- Mongo handles conversion from string to ObjectId
+        public string Id { get; set; }
+
+        [BsonElement("Name")] // this property represents the property called name in the db
+        [JsonProperty("Name")]
+        public string BookName { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+
+        public string Author { get; set; }
+    }
+}
